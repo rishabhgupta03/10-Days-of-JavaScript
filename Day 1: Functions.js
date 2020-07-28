77b51612be7f85a3513dc1ff5a3663a45bce24d5
Rishabh Gupta <44132253+rishabhgupta03@users.noreply.github.com>
2020-07-28 16:36:08 +0530

    Create Day 1: Functions.js

---
+++ Day 1: Functions.js
@@ -0,0 +1,38 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+ function factorial(n){
+     let fac = 1;
+     for (let i=2;i<=n;i++){
+         fac = fac*i;
+     }
+     return fac
+ }
+
+
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
